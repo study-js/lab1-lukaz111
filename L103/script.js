a4da46a63e6dade4fe5797735650506934f346f6
LumixGE <shotoo1@live.com>
2017-11-22 18:53:07 +0400

    martivi ricxvebi

--- L103/script.js
+++ L103/script.js
@@ -1,2 +1,30 @@
-var name ='დავითი', admin=name;
-alert(admin);
+while (true) {
+    var x = +prompt('martivi an ara', '');
+    var isPrime = true;
+    if (isNaN(x)) {
+        alert('arasworia');
+        continue
+    }
+    for (var i = 2; i < x; i++) {
+
+        if (x % i == 0) {
+
+            isPrime = false;
+            break;
+        }
+
+    }
+
+
+    if (isPrime == false) {
+        alert('არ არის მარტივი');
+    }
+    else {
+
+        alert('მარტივია');
+
+    }
+
+
+
+}
